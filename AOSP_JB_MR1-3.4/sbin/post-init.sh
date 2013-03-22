b420bbe5d3bf3bd2cd336023223f7acaa25db652
ktoonsez <ktoonsez@gmail.com>
2013-03-21 20:15:30 -0700

    Fix new min to 378 instead of 384

--- AOSP_JB_MR1-3.4/sbin/post-init.sh
+++ AOSP_JB_MR1-3.4/sbin/post-init.sh
@@ -1,7 +1,7 @@
 #!/system/bin/sh
 
 #Set governor items
-#echo 384000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq;
+#echo 378000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq;
 echo 1512000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq;
 
 DISLIT=0
